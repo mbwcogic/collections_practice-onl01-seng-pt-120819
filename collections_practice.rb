35b11d03ba50c8ecb80c7facca9c5ea38907b26a
Michael Waldo <mbwcogic@yahoo.com>
2019-12-30 03:13:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+end
+end 
 
+def sort_array_desc(array)
+  array.sort.reverse do |a, b|
+    b <=> a
+end 
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right| 
+    left.length <=> right.length
+  end 
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
